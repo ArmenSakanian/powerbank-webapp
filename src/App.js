c75c8898dd14d19ffad7230ce7f7bf6e604253a8
Armen Sakanian <sakanyan2004@gmail.com>
2025-04-05 00:00:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    if (window.Telegram.WebApp) {
+      window.Telegram.WebApp.ready(); // Сообщаем Telegram, что WebApp готов
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Powerbank аренда</h1>
+      <p>Добро пожаловать в мини-приложение!</p>
     </div>
   );
 }
